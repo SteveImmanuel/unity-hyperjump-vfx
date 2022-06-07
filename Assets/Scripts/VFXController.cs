5c9a5550f0f339211cbf8ff5e3751741c62b54df
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-06-07 14:34:51 +0900

    Add hyperjump controller

--- Assets/Scripts/VFXController.cs
+++ Assets/Scripts/VFXController.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VFXController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public VisualEffect hyperjump;
+
+    private float cooldown;
+    private float elapsed;
+
+    private void Awake()
     {
-        
+        elapsed = 0f;
+        cooldown = hyperjump.GetFloat("BeamLifetime") + 1.0f;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        ResetVFX();
+    }
+
     void Update()
     {
-        
+        elapsed += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hyperjump.pause = false;
+            elapsed = 0f;
+        }
+
+        if (elapsed >= cooldown)
+        {
+            ResetVFX();
+            elapsed = 0f;
+        }
+    }
+
+    private void ResetVFX()
+    {
+        hyperjump.gameObject.SetActive(false);
+        hyperjump.gameObject.SetActive(true);
+        hyperjump.Play();
+        hyperjump.pause = true;
     }
 }
