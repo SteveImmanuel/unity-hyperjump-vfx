b9c3c606eddb1c664aaef87c86b1cad4d6b81619
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-06-09 12:55:28 +0900

    Update script add postprocessing handler

--- Assets/Scripts/VFXController.cs
+++ Assets/Scripts/VFXController.cs
@@ -1,25 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using System;
 
 public class VFXController : MonoBehaviour
 {
+    public VolumeProfile globalVolume;
     public VisualEffect hyperjump;
+    public Renderer tunnelRenderer;
+    public AnimationCurve transitionCurve;
 
-    private float elapsed;
-    private float cooldown;
+    [Header("Lightspeed Tunnel Settings")]
+    public float brightestRadialScale = 0.5f;
+    public float normalRadialScale = 1.2f;
+
+    [Header("Postprocessing Settings")]
+    public float brightestBloom = 60f;
+    public float normalBloom = 1.5f;
+    public float activeDofStart = 800f;
+    public float normalDofStart = 1100f;
+
+    private bool inTunnel;
     private bool isPlaying;
+    private Bloom bloom;
+    private DepthOfField dof;
+    private float transitionDuration;
 
     private void Awake()
     {
+        inTunnel = false;
         isPlaying = false;
-        elapsed = 0f;
-        cooldown = hyperjump.GetFloat("BeamLifetime");
     }
 
     void Start()
     {
+        transitionDuration = hyperjump.GetFloat("BeamLifetime");
+        globalVolume.TryGet(out bloom);
+        globalVolume.TryGet(out dof);
         ResetVFX();
     }
 
@@ -27,19 +46,32 @@ public class VFXController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && !isPlaying)
         {
-            isPlaying = true;
-            hyperjump.pause = false;
-            elapsed = 0f;
-        }
 
-        if (elapsed > cooldown + 1f && isPlaying)
-        {
-            elapsed = 0;
-            isPlaying = false;
-            ResetVFX();
+            Invert(inTunnel);
+            if (!inTunnel)
+            {
+                isPlaying = true;
+                hyperjump.pause = false;
+                StartCoroutine(FadeInTransition(() => {
+                    StartCoroutine(FadeOutTransition(() => { 
+                        inTunnel = true;
+                        isPlaying = false;
+                        ResetVFX();
+                    })); 
+                }));
+            } 
+            else
+            {
+                isPlaying = true;
+                StartCoroutine(FadeInTransition(() => {
+                    hyperjump.pause = false;
+                    StartCoroutine(FadeOutTransition(() => {
+                        inTunnel = false;
+                        isPlaying = false;
+                    }));
+                }));
+            }
         }
-
-        elapsed += Time.deltaTime;
     }
 
     private void ResetVFX()
@@ -53,5 +85,55 @@ public class VFXController : MonoBehaviour
     private void Invert(bool invert)
     {
         hyperjump.SetBool("Invert", invert);
+    }
+
+    private IEnumerator FadeInTransition(Action callback = null)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < transitionDuration)
+        {
+            float frac = elapsed / transitionDuration;
+            float t = transitionCurve.Evaluate(frac);
+
+            bloom.intensity.value = Mathf.Lerp(normalBloom, brightestBloom, t);
+            tunnelRenderer.material.SetFloat("_RadialScale", Mathf.Lerp(normalRadialScale, brightestRadialScale, t));
+            dof.gaussianStart.value = Mathf.Lerp(normalDofStart, activeDofStart, t);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        bloom.intensity.value = brightestBloom;
+        tunnelRenderer.material.SetFloat("_RadialScale", brightestRadialScale);
+        dof.gaussianStart.value = activeDofStart;
+
+        tunnelRenderer.gameObject.SetActive(!tunnelRenderer.gameObject.activeInHierarchy);
+
+        callback?.Invoke();
+    }
+
+    private IEnumerator FadeOutTransition(Action callback = null)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < transitionDuration)
+        {
+            float frac = elapsed / transitionDuration;
+            float t = 1 - transitionCurve.Evaluate(1 - frac); ;
+
+            bloom.intensity.value = Mathf.Lerp(brightestBloom, normalBloom, t);
+            tunnelRenderer.material.SetFloat("_RadialScale", Mathf.Lerp(brightestRadialScale, normalRadialScale, t));
+            dof.gaussianStart.value = Mathf.Lerp(activeDofStart, normalDofStart, t);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        bloom.intensity.value = normalBloom;
+        tunnelRenderer.material.SetFloat("_RadialScale", normalRadialScale);
+        dof.gaussianStart.value = normalDofStart;
+
+        callback?.Invoke();
     }
 }
