d031b95c210c5cc06030a01a63a2584bc78cca48
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-06-10 14:18:59 +0900

    Fix ship rotation controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float movementSpeed = 5f;
     public float mouseSensitivity = 4f;
-    public float smoothTime = .5f;
     public float pitchMax;
     public float yawMax;
 
@@ -13,22 +11,17 @@ public class CameraController : MonoBehaviour
 
     private void Awake()
     {
-        yaw = transform.eulerAngles.y;
-        pitch = transform.eulerAngles.x;
+        yaw = transform.localEulerAngles.y;
+        pitch = transform.localEulerAngles.x;
     }
 
     private void Update()
     {
-        if (Input.GetButton("Fire2"))
-        {
-            yaw += mouseSensitivity * Input.GetAxis("Mouse X");
-            pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
-            Debug.Log("yaw"+yaw);
-            Debug.Log("pitch" + pitch);
+        yaw += mouseSensitivity * Input.GetAxis("Mouse X");
+        pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
 
-            yaw = Mathf.Clamp(yaw, -yawMax, yawMax);
-            pitch = Mathf.Clamp(pitch, -pitchMax, pitchMax);
-            transform.eulerAngles = new Vector3(pitch, yaw, 0);
-        }
+        yaw = Mathf.Clamp(yaw, -yawMax, yawMax);
+        pitch = Mathf.Clamp(pitch, -pitchMax, pitchMax);
+        transform.localEulerAngles = new Vector3(pitch, yaw, 0);
     }
 }
